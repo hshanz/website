24814fde92194218367171e1e9fd60870a6064eb
hshanz <david00gbg@hotmail.com>
2021-07-01 13:05:01 +0200

    Fixed custom assest for heroku

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,77 +1,83 @@
-import React,{useEffect, useRef} from 'react';
-import '../css/homepage.css'
+import React, { useEffect, useRef, useState } from "react";
+import "../css/homepage.css";
 import * as Three from "three";
-import data from '../assets/scene.json'
-
-
+//import data from '%PUBLIC_URL%/assets/scene.json'
 
 const HomePage = (props) => {
-    const animationRef = useRef(null)
-    let scene,camera,renderer,cup,light;
-    
-    useEffect(() => {
-     
-     initScene()
-     render()
-     animate()
-     
-    
-     window.addEventListener('wheel', onMouseWheel,{passive:false});
-     return () => animationRef.current.removeChild( renderer.domElement);
-     
-    },)
+  const animationRef = useRef(null);
+  let scene, camera, cup, light;
+  let renderer = null;
+  const [data, setData] = useState(null);
 
-    const initScene = () =>{
-     scene = new Three.Scene();
-     scene.background = new Three.Color(0x000000);
-
-     camera = new Three.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.3, 1000 );
-
-     renderer = new Three.WebGLRenderer({antialias:true,alpha:true});
-     renderer.setClearColor( 0x000000, 0 );
-     renderer.setSize( window.innerWidth, window.innerHeight );
-
-     animationRef.current.appendChild(renderer.domElement)
-     
-     let loader = new Three.ObjectLoader();
-     scene = loader.parse(data);
-     cup = scene.getObjectById(12);
-     light = scene.getObjectById(13);
-
-     cup.rotateX(0.5)
-     cup.position.set(0,1,0)
-     camera.position.set(0,1.4,7)  
-
+  useEffect(() => {
+    async function getScene() {
+      let response = await fetch(`${process.env.PUBLIC_URL}assets/scene.json`);
+      let info = await response.json();
+      setData(info);
     }
+    getScene();
+  }, []);
 
-    const onMouseWheel = (event) =>{
-        event.preventDefault();
-        cup.rotation.z += event.deltaY *0.5
-        render()
-    }
+  useEffect(() => {
+    const initScene = () => {
+      scene = new Three.Scene();
+      scene.background = new Three.Color(0x000000);
 
-    const render = () =>{
-        renderer.render( scene, camera );
-    }
-    
+      camera = new Three.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.3,1000);
+
+      renderer = new Three.WebGLRenderer({ antialias: true, alpha: true });
+      renderer.setClearColor(0x000000, 0);
+      renderer.setSize(window.innerWidth, window.innerHeight);
+
+      animationRef.current.appendChild(renderer.domElement);
+
+      let loader = new Three.ObjectLoader();
+      scene = loader.parse(data);
+      console.log(scene)
+      cup = scene.getObjectByName("Cup_0");
+      light = scene.getObjectByName("HemisphereLight");
+
+      cup.rotateX(0.5);
+      cup.position.set(0, 1, 0);
+      camera.position.set(0, 1.4, 7);
+    };
+
+    const onMouseWheel = (event) => {
+      event.preventDefault();
+      cup.rotation.z += event.deltaY * 0.5;
+      render();
+    };
+
+    const render = () => {
+      renderer.render(scene, camera);
+    };
+
     const animate = () => {
-        requestAnimationFrame( animate );
-        render()
-      };
-    
+      requestAnimationFrame(animate);
+      render();
+    };
 
-   
-    
-    return (
-            <div id="main">
-                <div id="canvas" ref={animationRef}> </div>
-                <div className="test"> Hello</div>
+    if (data !== null) {
+      initScene();
+      render();
+      animate();
 
-            </div>
-             
-        
-       
-    );
-}
+      window.addEventListener("wheel", onMouseWheel, { passive: false });
+    }
+
+    return () => {
+      if (renderer !== null) {
+        animationRef.current.removeChild(renderer.domElement);
+      }
+    };
+  }, [data]);
+
+  return (
+    <div id="main">
+      <div id="canvas" ref={animationRef}/>
+      <div className="test"> Hello</div>
+    </div>
+  );
+};
 
 export default HomePage;
